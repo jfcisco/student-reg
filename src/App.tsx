81092b05badc8e115a5dd4e52af9ddac7c080380
Jasper Francisco <jasper.francisco@gmail.com>
2021-05-27 19:57:58 +0800

    Refactor page logic into custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,35 @@ import Classes from './Pages/ClassesPage';
 import { useState } from 'react';
 import { Page, Route, RouteContext } from './Routing';
 
-function App(): JSX.Element {
-    const [currentPage, setCurrentPage] = useState<Page>(Page.Login);
-    const values = { currentPage, setCurrentPage };
+const usePageState = (initialPage: Page): [Page, (page: Page) => Promise<void>] => {
+  const [currentPage, setCurrentPage] = useState<Page>(initialPage);
 
-    return (
-        <RouteContext.Provider value={values}>
-            <main>
-                <Route page={Page.Login}>
-                    <Login />
-                </Route>
-                <Route page={Page.Classes}>
-                    <Classes />
-                </Route>
-            </main>
-        </RouteContext.Provider>
-    );
+  async function setCurrentPageAsync(page: Page) {
+    const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    await delay(1000);
+    setCurrentPage(page);
+  }
+
+  return [currentPage, setCurrentPageAsync];
+}
+
+function App(): JSX.Element {
+  const [currentPage, setCurrentPage] = usePageState(Page.Login);
+  const values = { currentPage, setCurrentPage };
+
+  return (
+    <RouteContext.Provider value={values}>
+      <main>
+        <Route page={Page.Login}>
+          <Login />
+        </Route>
+        <Route page={Page.Classes}>
+          <Classes />
+        </Route>
+      </main>
+    </RouteContext.Provider>
+  );
 }
 
 export default App;
