72be9b3a341124d2ea56a9f57eebc7e40473ead6
Jasper Francisco <jasper.francisco@gmail.com>
2021-05-28 18:17:47 +0800

    Add feedback for async operation

--- src/Pages/LoginForm.tsx
+++ src/Pages/LoginForm.tsx
@@ -1,30 +1,32 @@
 import React from 'react';
 
 type LoginFormProps = {
-    onSubmit(event: React.SyntheticEvent<HTMLFormElement>): void;
+  onSubmit(event: React.SyntheticEvent<HTMLFormElement>): void;
+  isLoading: boolean;
 }
-export function LoginForm({ onSubmit }: LoginFormProps): JSX.Element {
-    return (
-        <form className="flex flex-col bg-gray-100 shadow-md rounded-t-md py-8 px-10"
-            onSubmit={(e) => onSubmit(e)}>
-            <h2 className="font-bold text-blue-800 text-2xl mb-5">Log in to Student System</h2>
+export function LoginForm({ onSubmit, isLoading }: LoginFormProps): JSX.Element {
+  return (
+    <form className="flex flex-col bg-gray-100 shadow-md rounded-t-md py-8 px-10"
+      onSubmit={(e) => onSubmit(e)}>
+      <h2 className="font-bold text-blue-800 text-2xl mb-5">Log in to Student System</h2>
 
-            <div className="grid grid-cols-3 grid-rows-3 gap-y-3 mb-3">
-                <label htmlFor="username">Username:</label>
-                <input className="text-field col-span-2" type="text" name="username" />
+      <div className="grid grid-cols-3 grid-rows-3 gap-y-3 mb-3">
+        <label htmlFor="username">Username:</label>
+        <input className="text-field col-span-2" type="text" name="username" />
 
-                <label htmlFor="password">Password:</label>
-                <input className="text-field col-span-2" type="password" name="password" />
+        <label htmlFor="password">Password:</label>
+        <input className="text-field col-span-2" type="password" name="password" />
 
-                <label className="col-span-2 text-sm">
-                    <input type="checkbox" name="remember-me" />
+        <label className="col-span-2 text-sm">
+          <input type="checkbox" name="remember-me" />
                     &nbsp;Remember me?
                 </label>
-            </div>
+      </div>
 
-            <button
-                className="btn-submit"
-                type="submit">Log in</button>
-        </form>
-    );
+      <button
+        disabled={isLoading}
+        className="btn-submit"
+        type="submit">Log in</button>
+    </form>
+  );
 }
