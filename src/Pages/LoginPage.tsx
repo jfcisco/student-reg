cfe468faf3ed9e63383f1b09d2afa2a062d02493
Jasper Francisco <jasper.francisco@gmail.com>
2021-05-28 18:17:20 +0800

    Implement loading feedback for async operation

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,32 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import './LoginPage.css';
 import { Page, RouteContext } from "../Routing";
 import { LoginForm } from './LoginForm';
 
 function LoginPage(): JSX.Element {
-    const { setCurrentPage } = useContext(RouteContext);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { setCurrentPage } = useContext(RouteContext);
 
-    const handleSubmit = (e: React.SyntheticEvent) => {
-        e.preventDefault();
-        setCurrentPage(Page.Classes);
-    }
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    // TODO: Perform error handling or something
+    setCurrentPage(Page.Classes, () => setIsLoading(false));
+  }
 
-    return (
-        <div className="h-screen w-screen bg-blue-800 flex items-center text-gray-600">
-            <div className="w-3/12 mx-auto">
-                <header className="text-white text-center mb-7">
-                    <img className="mx-auto mb-2" src="https://via.placeholder.com/60x60" alt="university logo" />
-                    <h1 className="font-serif font-extralight text-2xl uppercase tracking-wider text-gray-200 ">Test University</h1>
-                </header>
+  return (
+    <div className="h-screen w-auto bg-blue-800 flex items-center text-gray-600">
+      <div className="mx-auto">
+        <header className="text-white text-center mb-6">
+          <img className="mx-auto mb-2" src="https://via.placeholder.com/60x60" alt="university logo" />
+          <h1 className="font-serif font-extralight text-3xl uppercase tracking-wider max-w-sm text-gray-200 ">
+            Not <br /> Ateneo de Manila University</h1>
+        </header>
 
-                <LoginForm onSubmit={(e) => handleSubmit(e)}/>
-                
-                <footer className="bg-gray-300 shadow-inner py-4 rounded-b-md text-center underline text-gray-500 text-sm">
-                    <a href="/forgot">Forgot your password?</a>
-                </footer>
-            </div>
-        </div>
-    );
+        <LoginForm onSubmit={(e) => handleSubmit(e)} isLoading={isLoading} />
+
+        <footer className="bg-gray-300 shadow-inner py-4 rounded-b-md text-center underline text-gray-500 text-sm">
+          <a href="/forgot">Forgot your password?</a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
